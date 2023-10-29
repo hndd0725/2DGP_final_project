512364f7fee6dab4f78a218fea6144f128913815
hndd0725 <hndd0725@naver.com>
2023-10-29 20:57:18 +0900

    투수 클래스 만들고 idle상태와 스페이스바누르면 던지는 모션 구현완료

--- control_boy.py
+++ control_boy.py
@@ -2,13 +2,8 @@ from pico2d import *
 
 import game_world
 from grass import Grass
-<<<<<<< HEAD
 from batter import Batter
 from pitcher import Pitcher
-=======
-from boy import Boy
-
->>>>>>> b1268b8fd92b7b43337f6f78e9264625113b4c53
 
 
 # Game object class here
@@ -24,18 +19,14 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         else:
-<<<<<<< HEAD
             hitter.handle_event(event)
             pitcher.handle_event(event)
-=======
-            boy.handle_event(event)
->>>>>>> b1268b8fd92b7b43337f6f78e9264625113b4c53
+
 
 
 def create_world():
     global running
     global grass
-<<<<<<< HEAD
     global pitcher
     global hitter
 
@@ -45,16 +36,11 @@ def create_world():
     game_world.add_object(hitter, 1)
     pitcher = Pitcher()
     game_world.add_object(pitcher, 1)
-=======
-    global team
-    global boy
+
 
     running = True
 
-    boy = Boy()
-    game_world.add_object(boy,1)
 
->>>>>>> b1268b8fd92b7b43337f6f78e9264625113b4c53
     grass = Grass()
     game_world.add_object(grass, 0)
 
@@ -78,10 +64,7 @@ while running:
     handle_events()
     update_world()
     render_world()
-<<<<<<< HEAD
     delay(0.3)
-=======
     delay(0.01)
->>>>>>> b1268b8fd92b7b43337f6f78e9264625113b4c53
 # finalization code
 close_canvas()
