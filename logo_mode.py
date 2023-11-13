8e9d40204c2409e1e6874e9f9e89469ce81a9325
hndd0725 <hndd0725@naver.com>
2023-11-14 01:14:42 +0900

    로고모드에서 플레이보드로 플레이모드에서 탑뷰모드로 변환할 수 있게 구현

---
+++ logo_mode.py
@@ -0,0 +1,28 @@
+from pico2d import get_events, load_image, clear_canvas, update_canvas, get_time
+import game_framework
+import play_mode
+
+
+def init():
+    global image
+    global logo_start_time
+    logo_start_time=get_time()
+    image = load_image('tuk_credit.png')
+    pass
+def finish():
+    pass
+def update():
+    if get_time()-logo_start_time>=2.0:
+        game_framework.change_mode(play_mode)
+        pass
+def draw():
+    clear_canvas()
+    image.draw(400,300)
+    update_canvas()
+    pass
+def handle_events():
+    events=get_events()
+def pause():
+    pass
+def resume():
+    pass
