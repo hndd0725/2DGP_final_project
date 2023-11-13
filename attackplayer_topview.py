771ebf058decb81451d7ba047dc2ea59d97eeedc
hndd0725 <hndd0725@naver.com>
2023-11-13 22:08:49 +0900

    탑뷰의 공격팀의 달리기 1루까지 가는 것 완료

--- attackplayer_topview.py
+++ attackplayer_topview.py
@@ -28,10 +28,16 @@ ACTION_PER_TIMEhit = 1.0 / TIME_PER_ACTIONhit
 FRAMES_PER_ACTIONhit = 6
 FRAMES_PER_ACTIONhit= FRAMES_PER_ACTIONhit * ACTION_PER_TIMEhit#액션프래임속
 #batteridle Action Speed
-TIME_PER_ACTION = 1
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 3
-FRAMES_PER_ACTION= FRAMES_PER_ACTION * ACTION_PER_TIME#액션프래임속
+TIME_PER_ACTIONrun = 0.5
+ACTION_PER_TIMErun = 1.0 / TIME_PER_ACTIONrun
+FRAMES_PER_ACTIONrun = 3
+FRAMES_PER_ACTIONrun= FRAMES_PER_ACTIONrun * ACTION_PER_TIMErun#액션프래임속
+#
+PIXEL_PER_METER = (5 / 0.15) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 1.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 class Idle:
 
@@ -46,13 +52,14 @@ class Idle:
 
     @staticmethod
     def do(batter):
-        batter.frame = (batter.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 1
+        batter.frame = (batter.frame + FRAMES_PER_ACTIONrun * ACTION_PER_TIMErun * game_framework.frame_time) % 1
 
     @staticmethod
     def draw(batter):
         # batter.image.clip_draw( 14, 0, 20, 20, batter.x, batter.y, 100, 150)
         # batter.image.clip_draw(34, 0, 17, 20, batter.x, batter.y, 100, 150)
-        batter.image.clip_draw(51, 0, 17, 20, batter.x, batter.y, 100, 150)
+        #batter.image.clip_draw(51, 0, 17, 20, batter.x, batter.y, 50, 60)
+        pass
 
 
 class Run:
@@ -68,25 +75,18 @@ class Run:
 
     @staticmethod
     def do(batter):
-        batter.frame = (batter.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+        batter.frame = (batter.frame + FRAMES_PER_ACTIONrun * ACTION_PER_TIMErun * game_framework.frame_time) % 3
         pass
 
     @staticmethod
     def draw(batter):
         match int(batter.frame):
             case 0:
-                batter.image.clip_draw( 14, 0, 20, 20, batter.x, batter.y, 100, 150)
+                batter.image.clip_draw( 14, 0, 20, 20, batter.x, batter.y, 20, 30)
             case 1:
-                batter.image.clip_draw(34, 0, 17, 20, batter.x, batter.y, 100, 150)
+                batter.image.clip_draw(34, 0, 17, 20, batter.x, batter.y, 20, 30)
             case 2:
-                batter.image.clip_draw(51, 0, 17, 20, batter.x, batter.y, 100, 150)
-
-
-
-
-
-
-
+                batter.image.clip_draw(51, 0, 17, 20, batter.x, batter.y, 20, 30)
 
 class StateMachine:
     def __init__(self, batter):
@@ -119,7 +119,7 @@ class StateMachine:
 
 class AtkPlayer:
     def __init__(self):
-        self.x, self.y = 320, 120
+        self.x, self.y = 400, -30#오른쪽 베이스490,60
         self.frame = 0
         self.action = 3#오른쪽idle
         self.dir = 0
@@ -128,9 +128,23 @@ class AtkPlayer:
         self.state_machine = StateMachine(self)
         self.state_machine.start()
         self.timer=0
+        self.i=0
+        self.t=0
+        self.situation=0
     def update(self):
         self.state_machine.update()
-
+        if self.situation == 0:
+            global ballhit_start_x, ballhit_start_y
+            self.t = self.i / 100
+            if self.i<50:
+                self.size = 60 * self.t+10
+            else:
+                self.size = 60 * (1-self.t)+10
+            self.x = (1 - self.t) * 400 + self.t * 490
+            self.y = (1 - self.t) * -30 + self.t * 60
+            self.i += 1 * RUN_SPEED_PPS * game_framework.frame_time
+            if self.t >= 1:
+                self.situation=-1
     def handle_event(self, event):
         self.state_machine.handle_event(('INPUT', event))
 
